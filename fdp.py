dce4e320f533bfd1917a9b4e206c154d1e17a029
Morten Wang <nettrom@gmail.com>
2018-07-10 10:28:56 -0700

    Add acceleration values to TSV output.
    We grabbed the acceleration values for all three axes correctly, but
    did not include them in the properties in the TSV output. This adds
    them in the correct place, right after the engine RPM values.
    
    Also added a utility function for writing a tab-separated list of
    column names for use as a file header, which will be added later.
    
    Closes issue #1

--- fdp.py
+++ fdp.py
@@ -33,7 +33,35 @@ class ForzaDataPacket:
     def __init__(self, data):
         ## Format string that allows unpack to process the data bytestream:
         forza_format = '<iIfffffffffffffffffffffffffffffffffffffffffffffffffffiiiii'
-
+        self._parameter_list = [
+            'is_race_on', 'timestamp_ms',
+            'engine_max_rpm', 'engine_idle_rpm', 'current_engine_rpm',
+            'acceleration_x', 'acceleration_y', 'acceleration_z',
+            'velocity_x', 'velocity_y', 'velocity_z',
+            'angular_velocity_x', 'angular_velocity_y', 'angular_velocity_z',
+            'yaw', 'pitch', 'roll',
+            'norm_suspension_travel_FL', 'norm_suspension_travel_FR',
+            'norm_suspension_travel_RL', 'norm_suspension_travel_RR',
+            'tire_slip_ratio_FL', 'tire_slip_ratio_FR',
+            'tire_slip_ratio_RL', 'tire_slip_ratio_RR',
+            'wheel_rotation_speed_FL', 'wheel_rotation_speed_FR',
+            'wheel_rotation_speed_RL', 'wheel_rotation_speed_RR',
+            'wheel_on_rumble_strip_FL', 'wheel_on_rumble_strip_FR',
+            'wheel_on_rumble_strip_RL', 'wheel_on_rumble_strip_RR',
+            'wheel_in_puddle_FL', 'wheel_in_puddle_FR',
+            'wheel_in_puddle_RL', 'wheel_in_puddle_RR',
+            'surface_rumble_FL', 'surface_rumble_FR',
+            'surface_rumble_RL', 'surface_rumble_RR',
+            'tire_slip_angle_FL', 'tire_slip_angle_FR',
+            'tire_slip_angle_RL', 'tire_slip_angle_RR',
+            'tire_combined_slip_FL', 'tire_combined_slip_FR',
+            'tire_combined_slip_RL', 'tire_combined_slip_RR',
+            'suspension_travel_meters_FL', 'suspension_travel_meters_FR',
+            'suspension_travel_meters_RL', 'suspension_travel_meters_RR',
+            'car_ordinal', 'car_class', 'car_performance_index',
+            'drivetrain_type', 'num_cylinders'
+            ]
+        
         (self.is_race_on, self.timestamp_ms,
          self.engine_max_rpm, self.engine_idle_rpm, self.current_engine_rpm,
          self.acceleration_x, self.acceleration_y, self.acceleration_z,
@@ -60,6 +88,12 @@ class ForzaDataPacket:
          self.suspension_travel_meters_RL, self.suspension_travel_meters_RR,
          self.car_ordinal, self.car_class, self.car_performance_index,
          self.drivetrain_type, self.num_cylinders) = unpack(forza_format, data)
+
+    def get_tsv_header(self):
+        '''
+        Return a tab-separated string with the names of all properties in the order defined in the data packet.
+        '''
+        return('\t'.join(self._parameter_list))
         
     def to_tsv(self):
         '''
@@ -67,4 +101,4 @@ class ForzaDataPacket:
         All floating point numbers are defined as such to allow for changing
         the number of significant digits if desired.
         '''
-        return('{0.is_race_on}\t{0.timestamp_ms}\t{0.engine_max_rpm:f}\t{0.engine_idle_rpm:f}\t{0.current_engine_rpm:f}\t{0.velocity_x:f}\t{0.velocity_y:f}\t{0.velocity_z:f}\t{0.angular_velocity_x:f}\t{0.angular_velocity_y:f}\t{0.angular_velocity_z:f}\t{0.yaw:f}\t{0.pitch:f}\t{0.roll:f}\t{0.norm_suspension_travel_FL:f}\t{0.norm_suspension_travel_FR:f}\t{0.norm_suspension_travel_RL:f}\t{0.norm_suspension_travel_RR:f}\t{0.tire_slip_ratio_FL:f}\t{0.tire_slip_ratio_FR:f}\t{0.tire_slip_ratio_RL:f}\t{0.tire_slip_ratio_RR:f}\t{0.wheel_rotation_speed_FL:f}\t{0.wheel_rotation_speed_FR:f}\t{0.wheel_rotation_speed_RL:f}\t{0.wheel_rotation_speed_RR:f}\t{0.wheel_on_rumble_strip_FL:f}\t{0.wheel_on_rumble_strip_FR:f}\t{0.wheel_on_rumble_strip_RL:f}\t{0.wheel_on_rumble_strip_RR:f}\t{0.wheel_in_puddle_FL:f}\t{0.wheel_in_puddle_FR:f}\t{0.wheel_in_puddle_RL:f}\t{0.wheel_in_puddle_RR:f}\t{0.surface_rumble_FL:f}\t{0.surface_rumble_FR:f}\t{0.surface_rumble_RL:f}\t{0.surface_rumble_RR:f}\t{0.tire_slip_angle_FL:f}\t{0.tire_slip_angle_FR:f}\t{0.tire_slip_angle_RL:f}\t{0.tire_slip_angle_RR:f}\t{0.tire_combined_slip_FL:f}\t{0.tire_combined_slip_FR:f}\t{0.tire_combined_slip_RL:f}\t{0.tire_combined_slip_RR:f}\t{0.suspension_travel_meters_FL:f}\t{0.suspension_travel_meters_FR:f}\t{0.suspension_travel_meters_RL:f}\t{0.suspension_travel_meters_RR:f}\t{0.car_ordinal}\t{0.car_class}\t{0.car_performance_index}\t{0.drivetrain_type}\t{0.num_cylinders}'.format(self))
+        return('{0.is_race_on}\t{0.timestamp_ms}\t{0.engine_max_rpm:f}\t{0.engine_idle_rpm:f}\t{0.current_engine_rpm:f}\t{0.acceleration_x:f}\t{0.acceleration_y:f}\t{0.acceleration_z:f}\t{0.velocity_x:f}\t{0.velocity_y:f}\t{0.velocity_z:f}\t{0.angular_velocity_x:f}\t{0.angular_velocity_y:f}\t{0.angular_velocity_z:f}\t{0.yaw:f}\t{0.pitch:f}\t{0.roll:f}\t{0.norm_suspension_travel_FL:f}\t{0.norm_suspension_travel_FR:f}\t{0.norm_suspension_travel_RL:f}\t{0.norm_suspension_travel_RR:f}\t{0.tire_slip_ratio_FL:f}\t{0.tire_slip_ratio_FR:f}\t{0.tire_slip_ratio_RL:f}\t{0.tire_slip_ratio_RR:f}\t{0.wheel_rotation_speed_FL:f}\t{0.wheel_rotation_speed_FR:f}\t{0.wheel_rotation_speed_RL:f}\t{0.wheel_rotation_speed_RR:f}\t{0.wheel_on_rumble_strip_FL:f}\t{0.wheel_on_rumble_strip_FR:f}\t{0.wheel_on_rumble_strip_RL:f}\t{0.wheel_on_rumble_strip_RR:f}\t{0.wheel_in_puddle_FL:f}\t{0.wheel_in_puddle_FR:f}\t{0.wheel_in_puddle_RL:f}\t{0.wheel_in_puddle_RR:f}\t{0.surface_rumble_FL:f}\t{0.surface_rumble_FR:f}\t{0.surface_rumble_RL:f}\t{0.surface_rumble_RR:f}\t{0.tire_slip_angle_FL:f}\t{0.tire_slip_angle_FR:f}\t{0.tire_slip_angle_RL:f}\t{0.tire_slip_angle_RR:f}\t{0.tire_combined_slip_FL:f}\t{0.tire_combined_slip_FR:f}\t{0.tire_combined_slip_RL:f}\t{0.tire_combined_slip_RR:f}\t{0.suspension_travel_meters_FL:f}\t{0.suspension_travel_meters_FR:f}\t{0.suspension_travel_meters_RL:f}\t{0.suspension_travel_meters_RR:f}\t{0.car_ordinal}\t{0.car_class}\t{0.car_performance_index}\t{0.drivetrain_type}\t{0.num_cylinders}'.format(self))
